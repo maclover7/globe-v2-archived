ac7467270ede0eabd600ef0a94dad52a18ae43ef
Jon Moss <me@jonathanmoss.me>
2015-11-01 21:42:39 -0500

    ember-simple-auth integration is working... standby Rails::API api!

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -1,18 +1,22 @@
 import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend({
-  actions: {
-    signInGoogle: function(){
-      var controller = this.controllerFor('application');
-      this.get('torii').open('google-oauth2').then(function(authorization){
-        var auth_code = authorization.authorizationCode;
-        // Get User Data
-        var userData = 'hi';
-        debugger;
-        // create user
-        controller.set('hasGoogle', true);
-      });
-    },
-  }
-});
+export default Ember.Route.extend(ApplicationRouteMixin);
 
+//import Ember from 'ember';
+
+//export default Ember.Route.extend({
+  //actions: {
+    //signInGoogle: function(){
+      //var controller = this.controllerFor('application');
+      //this.get('torii').open('google-oauth2').then(function(authorization){
+        //var auth_code = authorization.authorizationCode;
+        //// Get User Data
+        //var userData = 'hi';
+        //debugger;
+        //// create user
+        //controller.set('hasGoogle', true);
+      //});
+    //},
+  //}
+//});
